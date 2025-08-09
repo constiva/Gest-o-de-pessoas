fa07f5df20884f4ff6921b776bab62516671b3e4
constiva <grupo.constiva@gmail.com>
2025-08-09 13:32:53 -0300

    fix: constrain employee table width

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex">
       <Sidebar />
-      <main className="flex-1 p-6 bg-white min-h-screen">{children}</main>
+      <main className="flex-1 min-w-0 p-6 bg-white min-h-screen">{children}</main>
     </div>
   );
 }
