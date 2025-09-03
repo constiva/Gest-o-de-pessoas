a5965c722ece9b1e88a04519f2c21e03f1a0e6b6
constiva <grupo.constiva@gmail.com>
2025-09-03 20:32:44 -0300

    fix: show only percentages in talent origin pie

--- pages/recruitment/index.tsx
+++ pages/recruitment/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Layout from '../../components/Layout';
 import JobTab from '../../components/recruitment/JobTab';
 import TalentTab from '../../components/recruitment/TalentTab';
+import RecruitmentMetrics from '../../components/recruitment/RecruitmentMetrics';
 import {
   Tabs,
   TabsList,
@@ -21,9 +22,7 @@ export default function Recruitment() {
           <TabsList className="mb-4">
             <TabsTrigger value="jobs">Vagas</TabsTrigger>
             <TabsTrigger value="talents">Banco de Talentos</TabsTrigger>
-            <TabsTrigger value="pipeline">Pipeline Global</TabsTrigger>
-            <TabsTrigger value="reports">Relatórios</TabsTrigger>
-            <TabsTrigger value="exports">Exportações</TabsTrigger>
+            <TabsTrigger value="metrics">Métricas</TabsTrigger>
           </TabsList>
           <TabsContent value="jobs">
             <JobTab />
@@ -31,14 +30,8 @@ export default function Recruitment() {
           <TabsContent value="talents">
             <TalentTab />
           </TabsContent>
-          <TabsContent value="pipeline">
-            <p>Pipeline Global em construção.</p>
-          </TabsContent>
-          <TabsContent value="reports">
-            <p>Relatórios em construção.</p>
-          </TabsContent>
-          <TabsContent value="exports">
-            <p>Exportações em construção.</p>
+          <TabsContent value="metrics">
+            <RecruitmentMetrics />
           </TabsContent>
         </Tabs>
       </Layout>
